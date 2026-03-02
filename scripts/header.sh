04ef7e34bbe8e0e8fb1c5558b5f2eb96bb970b69
Alonso VPR <djcrawler@gmail.com>
2026-03-02 12:18:44 -0400

    Update header.sh

--- scripts/header.sh
+++ scripts/header.sh
@@ -32,7 +32,7 @@ title() {
     local largo=${#texto}
     local linea=$(printf '%*s' "$largo" | tr ' ' '-')
     echo -e "\n${BOLD_YELLOW}${texto}"
-    echo -e "${linea}${RESET}"
+    echo -e "${linea}-${RESET}"
 }
 
 # NVM Bridge
